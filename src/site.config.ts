a6d748af19710ca42549379932cf4317c392ecf4
Ricardo Ibaven <ricardoibaven25@gmail.com>
2025-04-26 16:12:33 -0600

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "CIELO-G",
+	author: "Mind Mesh",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: "en-GB",
@@ -14,13 +14,13 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "CIELO-G learning resources",
+	description: "A Gamified Health Hub for Latine Youth",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "en_GB",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "CIELO-G Learning",
+	title: "HealthInnoAI",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
 	url: "https://healthinnoai.github.io/",
 };
